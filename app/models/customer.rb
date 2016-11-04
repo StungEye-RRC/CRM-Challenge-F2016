d92f28a0ec7a6638a7ce02984c8d148000496aed
Kyle Geske <stungeye@gmail.com>
2016-11-04 15:15:58 -0500

    Added name and image validations.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  validates :full_name, presence: true
+  validates :image, format: { with: /.+\.(png|jpg|jpeg|gif)\z/,
+                              message: 'Filename must be of type png, jpg or gif.'}
 end
