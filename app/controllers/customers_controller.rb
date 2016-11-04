69391c6cfd5a6a05f408e88ef9b830c095b19b4f
Kyle Geske <stungeye@gmail.com>
2016-11-04 15:54:29 -0500

    New/Create and Edit/Update no longer link to or redirect to scaffolded views.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -56,7 +56,7 @@ class CustomersController < ApplicationController
   def destroy
     @customer.destroy
     respond_to do |format|
-      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Customer was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
